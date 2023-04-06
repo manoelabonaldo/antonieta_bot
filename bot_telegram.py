2625df169f1455ca150dcd6b29c5119ff9b78685
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-04-06 19:51:46 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -17,7 +17,7 @@ with open("credenciais.json", mode="w") as arquivo:
   arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
 conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
 api = gspread.authorize(conta)
-planilha = api.open_by_key["GOOGLE_SHEETS_KEY"]
+planilha = api.open_by_key(f'{GOOGLE_SHEETS_KEY}')
 sheet = planilha.worksheet("Página1")
 
 def bot_dotelegram(update): 
