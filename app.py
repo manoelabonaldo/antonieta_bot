7e046772970674be994b8135aca5a1747517cf57
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-04-06 19:19:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,3 +52,8 @@ def telegram_bot():
   update = request.json
   bot_telegram.bot_dotelegram(update)
   return "ok"
+
+@app.route("/salva-lista")
+def arquivolistasuja():
+  return menu + "Aqui vai o conteúdo de arquivo da lista suja"
+
