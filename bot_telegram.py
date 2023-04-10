64f7b6b36f6c627e9882ca0c9ec0653559064216
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-04-10 09:58:49 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -102,6 +102,6 @@ def bot_dotelegram(update):
    nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
    resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data = nova_mensagem)
     # Atualiza planilha do sheets com último update processado
-   sheet.append_row([first_name, last_name, message,sender_id, date])
+   sheet.append_row([first_name, last_name, sender_id, message, date])
 
    return "ok"
